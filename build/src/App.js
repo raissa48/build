d323ac53c27b04d55854f95633d468e966870780
RAISSA FRANCISCO LIMA <00001095655085sp@aluno.educacao.sp.gov.br>
2023-03-21 18:49:18 -0300

    meu 1 commit

---
+++ build/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function App(){
+  return(
+    <div>
+        <Cabecalho/>
+          <Texto/>
+        <Cabecalho/>
+    </div>
+  )
+}
+
+export function Cabecalho(){
+  return(
+    <div style={{
+      backgroundColor:'black', 
+      height:'20vh'
+    }}
+    >
+      
+    </div>
+  )
+}
+
+export function Texto(){
+  return(
+    <div style={{
+      paddingBottom:'20vh',  
+      paddingTop:'20vh'
+    }}>
+       <h1>Jorge the Cat</h1>
+    </div>
+  )
+}
